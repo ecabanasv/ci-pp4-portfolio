c3b3dd936d8019a8bbb0cd0ebc2d10c9d8af0889
kikiusen <ecvoracle@gmail.com>
2022-12-06 16:38:31 +0000

    Added class ProjectListView

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Project
 
 # Create your views here.
+
+class ProjectListView(generic.ListView):
+    """
+    Class based view for the projects
+    """
+    model = Project
+    template_name = 'portfolio.html'
+    context_object_name = 'projects'
+
+    def get_queryset(self):
+        """
+        Return the published projects
+        """
+        return Project.objects.filter(published=1).order_by('-created_on')
