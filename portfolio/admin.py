6a60ea03bf55046f74679c9fbf046e0d012a8ff9
kikiusen <ecvoracle@gmail.com>
2022-12-06 15:57:51 +0000

    Project admin: Added Prepop Slug, Search, Filter and Display

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,4 @@
+"""Portfolio Admin"""
 from django.contrib import admin
 from .models import Project, Comment
 from django_summernote.admin import SummernoteModelAdmin
@@ -8,5 +9,14 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Project)
 
 class ProjectAdmin(SummernoteModelAdmin):
+    """Admin View for Project"""
+
+    prepopulated_fields = {'slug': ['title']}
+
+    list_display = ('title', 'slug', 'published', 'created_on')
+
+    list_filter = ('published',)
+
+    search_fields = ['title', 'content']
 
     summernote_fields = ('description')
