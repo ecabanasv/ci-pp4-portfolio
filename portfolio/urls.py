a5650dade5c5fe31b8ef5ccfb1d91edf3c5cf993
kikiusen <ecvoracle@gmail.com>
2022-12-07 23:32:43 +0000

    Added paths: about, portfolio and contact

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.ProjectListView.as_view(), name='portfolio'),
+    path('about/', views.about, name='about'),
+    path('portfolio/', views.ProjectListView.as_view(), name='portfolio'),
+    path('contact/', views.contact, name='contact'),
 ]
