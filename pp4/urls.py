1902150969684e9524872a66ac084024c5a9ad5d
kikiusen <ecvoracle@gmail.com>
2022-12-07 20:14:44 +0000

    Added path: portfolio and allauth

--- pp4/urls.py
+++ pp4/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('portfolio.urls'), name='portfolio'),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
