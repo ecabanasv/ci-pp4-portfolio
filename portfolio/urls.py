bf9fe677300521f19d0a44ce15e61cfcd6eae5e7
kikiusen <ecvoracle@gmail.com>
2022-12-08 22:15:55 +0000

    Updated paths: portfolio

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('portfolio/', views.ProjectListView.as_view(), name='portfolio_urls'),
+    path('portfolio/', views.ProjectListView.as_view(), name='portfolio'),
     path('contact/', views.contact, name='contact'),
     path('portfolio/<slug:slug>/', views.ProjectDetailView.as_view(), name='project_detail'),
 ]
