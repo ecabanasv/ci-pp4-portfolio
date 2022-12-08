bb62c810561c851df123215b601dccacdac467ae
kikiusen <ecvoracle@gmail.com>
2022-12-08 20:50:11 +0000

    Added crispy_forms and crispy_bootstrap5 template

--- pp4/settings.py
+++ pp4/settings.py
@@ -48,7 +48,9 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
-    'django_summernote',	
+    'django_summernote',
+    'crispy_forms',
+    "crispy_bootstrap5",	
     'portfolio',
 ]
 
@@ -56,6 +58,9 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
