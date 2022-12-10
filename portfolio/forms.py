02aba550717708529c0d7f447d8ead98fd2b6112
kikiusen <ecvoracle@gmail.com>
2022-12-10 15:40:27 +0000

    Updated Meta class: fields and widgets

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,7 +1,16 @@
-from .models import Comment
+"""Forms for the portfolio app"""
 from django import forms
+from .models import Comment
 
 class CommentForm(forms.ModelForm):
+    """Form for the comments"""
     class Meta:
+        """Meta class for the comments"""
         model = Comment
-        fields = ('name', 'email', 'text_pros', 'text_cons', 'score')
+        fields = ('name', 'text_pros', 'text_cons', 'score',)
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'text_pros': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Pros'}),
+            'text_cons': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Cons'}),
+            'score': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Rate'}),
+        }
