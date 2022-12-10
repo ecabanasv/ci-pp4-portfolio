bc6f823fdc921bc8af7f3957a0df2160ef1ae3a5
kikiusen <ecvoracle@gmail.com>
2022-12-10 15:42:06 +0000

    Added paths: project_create, project_update and project_delete

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,6 +7,9 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('portfolio/', views.ProjectListView.as_view(), name='portfolio'),
     path('contact/', views.contact, name='contact'),
+    path('portfolio/project_create/', views.ProjectCreateView.as_view(), name='project_create'),
+    path('portfolio/project_update/<slug:slug>/', views.ProjectUpdateView.as_view(), name='project_update'),
+    path('portfolio/project_delete/<slug:slug>/', views.ProjectDeleteView.as_view(), name='project_delete'),
     path('portfolio/<slug:slug>/', views.ProjectDetailView.as_view(), name='project_detail'),
     path('portfolio/like/<slug:slug>', views.ProjectLikeView.as_view(), name='like_project'),
 ]
