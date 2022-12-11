2796b5d652956c1e7162d37495707eca16a4adbb
ecabanasv <ecvoracle@gmail.com>
2022-12-11 22:14:49 +0000

    Format document

--- portfolio/models.py
+++ portfolio/models.py
@@ -8,10 +8,13 @@ from cloudinary.models import CloudinaryField
 
 PUBLISH = ((0, "NO"), (1, "YES"))
 RATE = ((0, "0"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5"))
+
+
 class Project(models.Model):
     """
     Model for the project
     """
+
     title = models.CharField(max_length=250, unique=True)
     slug = models.SlugField(max_length=250, unique=True)
     author = models.ForeignKey(
@@ -20,20 +23,20 @@ class Project(models.Model):
     image_main = CloudinaryField("image", default="placeholder")
     excerpt = models.TextField(blank=True)
     description = models.TextField()
-    github_url = models.URLField(max_length = 200)
-    live_url = models.URLField(max_length = 200)
+    github_url = models.URLField(max_length=200)
+    live_url = models.URLField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     published = models.IntegerField(choices=PUBLISH, default=0)
     likes = models.ManyToManyField(User, related_name="project_likes", blank=True)
 
-
-# Meta class for ordering projects by date
+    # Meta class for ordering projects by date
 
     class Meta:
         """
         Meta class for ordering projects by date
         """
+
         ordering = ["-created_on"]
 
     def __str__(self):
@@ -48,10 +51,12 @@ class Project(models.Model):
 
 # Comments model
 
+
 class Comment(models.Model):
     """
     Model for the comments
     """
+
     project = models.ForeignKey(
         Project, on_delete=models.CASCADE, related_name="comments"
     )
@@ -63,23 +68,27 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     published = models.BooleanField(default=False)
 
-
     class Meta:
         """
         Meta class for ordering comments by date
         """
+
         ordering = ["created_on"]
 
     def __str__(self):
-        return ("Pros: {self.text_pros}" +
-                "Cons: {self.text_cons}"+
-                "Score: {self.score}"+
-                "By: {self.name}")
+        return (
+            "Pros: {self.text_pros}"
+            + "Cons: {self.text_cons}"
+            + "Score: {self.score}"
+            + "By: {self.name}"
+        )
+
 
 class Contact(models.Model):
     """
     Model for the contact
     """
+
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=100)
