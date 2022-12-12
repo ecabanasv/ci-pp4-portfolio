24c7d50f7a32e2fbaf81086e39453eabf13e40c7
kikiusen <ecvoracle@gmail.com>
2022-12-12 15:15:10 +0000

    DEBUG = False

--- pp4/settings.py
+++ pp4/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pp4-portfolio.herokuapp.com', 'localhost', '127.0.0.1']
 
